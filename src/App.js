6ca4dc02e3304165186fd7947f7c72e233071e62
René H <hasertrene@gmail.com>
2020-02-27 16:38:13 +0100

    asdf

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Switch, Route, Link, NavLink } from "react-router-dom";
+import AboutPage from './pages/AboutPage'
+import HomePage from './pages/HomePage'
+import DiscoverMoviesPage from './pages/DiscoverMoviesPage'
 
 function App() {
+
+
   return (
     <div className="App">
-      American Dicking
+        <nav>
+          <NavLink to="/HomePage">home</NavLink>
+          <Link to="/AboutPage">about</Link>
+          <Link to="/DiscoverMoviesPage">discover movies</Link>
+        </nav>
+        <Switch>
+        <Route path="/discover" component={DiscoverMoviesPage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/" exact={true} component={HomePage} />
+      </Switch>
     </div>
   );
 }
